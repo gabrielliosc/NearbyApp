fadb9b69ac5e8dfa387e8bb8f20ab08832742ade
Gabrielli <gabrielli.osc@gmail.com>
2024-12-09 20:06:28 -0300

    Initial Commit

---
+++ NearbyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearbyApp
+//
+//  Created by Gabi on 09/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
